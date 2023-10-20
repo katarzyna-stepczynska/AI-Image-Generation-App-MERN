37f7904dc727ccac2fc2a01c8635dcb1be4443c5
KStepczyńska <stepczynska.katarzyna@gmail.com>
2023-10-20 21:54:45 +0200

    connect frontend and backend with generateImage function in CreatePost page

--- server/routes/dalleRoutes.js
+++ server/routes/dalleRoutes.js
@@ -1,17 +1,11 @@
 import express from "express";
 import * as dotenv from "dotenv";
-// import { Configuration, OpenAIApi } from "openai";
 import OpenAI from "openai";
 
 dotenv.config();
 
 const router = express.Router();
 
-// const configuration = new Configuration({
-//   apiKey: process.env.OPENAI_API_KEY,
-// });
-
-// const openai = new OpenAIApi(configuration);
 const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
 });
@@ -23,18 +17,20 @@ router.route("/").get((req, res) => {
 router.route("/").post(async (req, res) => {
   try {
     const { prompt } = req.body;
-    const aiResponse = await openai.createImage({
+    const aiResponse = await openai.images.generate({
       prompt,
       n: 1,
       size: "1024x1024",
       response_format: "b64_json",
     });
 
-    const image = aiResponse.data.data[0].b64_json;
+    const image = aiResponse.data[0].b64_json;
     res.status(200).json({ photo: image });
   } catch (error) {
     console.log(error);
-    res.status(500).send(error?.response.data.error.message);
+    res
+      .status(500)
+      .send(error?.response.data.error.message || "Something went wrong");
   }
 });
 
